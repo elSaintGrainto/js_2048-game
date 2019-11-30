ace11d87f922b41e19e6d1991df0f149a9004da7
elsaintgrainto <lol.juanmgo@gmail.com>
2019-11-30 16:55:13 -0300

    changed the class of main panel

--- js/display.js
+++ js/display.js
@@ -5,18 +5,12 @@
  * then add a transition effect when moved from position
  */
 //make a big square for main panel, then small squares for position
-let div = document.createElement("div").classList.add("main-panel");
+let div = document.createElement("div").classList.add("sqr-panel");
 let sqrBg = document.createElement("div").classList.add("sqr-floor");
-let sizeX, sizeY, sizeSqr = 50;
+let sizeSqr = 50;
 let posX = 0,
     posY = 0;
 //maybe use grid or flexbox to position it the squares
-for (let i = 0; i < sizeX; i++) {
-
-    for (let j = 0; j < sizeY; j++) {
-        let d = document.createElement("div");
-
-        div.appendChild(d);
-
-    }
-}
+/**
+ * generate divs with sqr-floor class
+ */
