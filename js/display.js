ec616b7646cae92c506c06a7db7360c5b6f28734
elsaintgrainto <lol.juanmgo@gmail.com>
2019-11-30 17:10:13 -0300

    success added sqr-panel, and generating the floor

--- js/display.js
+++ js/display.js
@@ -5,24 +5,27 @@
  * then add a transition effect when moved from position
  */
 //make a big square for main panel, then small squares for position
-let sqrPanel = document.createElement("div").classList.add("sqr-panel");
-let sqrBg = document.createElement("div").classList.add("sqr-floor");
+let sqrPanel = document.createElement("div");
+
 let sizeSqr = 50;
 let posX = 0,
     posY = 0;
 //maybe use grid or flexbox to position it the squares
 /**
- * generate divs with sqr-floor class
+ * Generate a matrix of squares with "floor" class in it and apped them on sqr-panel
+ * @param {Integer} size large of matrix
  */
 function generateFloor(size) {
+    sqrPanel.classList.add("sqr-panel");
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-
             let d = document.createElement("div");
-
-
+            d.classList.add("square");
+            d.classList.add("floor");
             sqrPanel.appendChild(d);
-
+            console.log("doing thiing");
         }
     }
+    document.getElementById("main-panel").appendChild(sqrPanel);
 }
+generateFloor(4);
