dc0f1edf29583c2f6359a81f1bc336cf9e53f50b
elsaintgrainto <lol.juanmgo@gmail.com>
2019-11-30 16:06:35 -0300

    trying to do loop for set te squares

--- js/display.js
+++ js/display.js
@@ -7,3 +7,16 @@
 //make a big square for main panel, then small squares for position
 let div = document.createElement("div").classList.add("main-panel");
 let sqrBg = document.createElement("div").classList.add("sqr-floor");
+let sizeX, sizeY, sizeSqr = 50;
+let posX = 0,
+    posY = 0;
+//maybe use grid or flexbox to position it the squares
+for (let i = 0; i < sizeX; i++) {
+
+    for (let j = 0; j < sizeY; j++) {
+        let d = document.createElement("div");
+
+        div.appendChild(d);
+
+    }
+}
